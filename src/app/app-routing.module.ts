6dc7a4026f1cb0813584b0b78089ea5e5f018a3f
vaishali <waghe.vaishali1991@gmail.com>
2023-04-15 21:10:12 +0530

    save changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
